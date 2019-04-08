5e79866ab8d0ed97763e87d8523d9a8435b3887b
Jockey66666 <weisunglee@gmail.com>
2019-04-08 15:53:13 +0800

    modify exit code for Macos, should be 0~255

--- ErrorCodeDef.go
+++ ErrorCodeDef.go
@@ -2,11 +2,11 @@ package main
 
 const (
 	// ErrorOpenBankJSONFailed : open bank.json failed.
-	ErrorOpenBankJSONFailed int = 10001
+	ErrorOpenBankJSONFailed int = 1
 	// ErrorOpenBankListFailed : open input_bank_list.json failed.
-	ErrorOpenBankListFailed int = 10002
+	ErrorOpenBankListFailed int = 2
 	// ErrorOpenFxListFailed : open fxlist.json failed.
-	ErrorOpenFxListFailed int = 10003
+	ErrorOpenFxListFailed int = 3
 	// ErrorOpenAmpListFailed : open amplist.json failed.
-	ErrorOpenAmpListFailed int = 10004
+	ErrorOpenAmpListFailed int = 4
 )
