22eb7a472354bb6cb2725e491b33bd1a120cbfe9
Jockey66666 <weisunglee@gmail.com>
2019-04-16 18:43:26 +0800

    clean repo folder and modify build output path

--- build/build_windows.bat
+++ build/build_windows.bat
@@ -1,14 +1,17 @@
 SET CGO_ENABLED=0
 SET GOOS=windows
 SET GOARCH=386
-go build -o PresetConverterWin32.exe ../
+go build -o ./Windows/x86/PresetConverterWin.exe ../src/
 
 SET CGO_ENABLED=0
 SET GOOS=windows
 SET GOARCH=amd64
-go build -o PresetConverterWin64.exe ../
+go build -o ./Windows/x64/PresetConverterWin.exe ../src/
 
 SET CGO_ENABLED=0
 SET GOOS=darwin
 SET GOARCH=amd64
-go build -o PresetConverterMac ../
+go build -o ./Mac/PresetConverterMac ../src/
+
+cp ../src/thumbnail.png ./Windows/
+cp ../src/thumbnail.png ./Mac/
