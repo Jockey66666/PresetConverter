20137957ca862ee00537e664723fba6fbde02a2c
Jockey66666 <weisunglee@gmail.com>
2019-05-06 13:21:28 +0800

    fix classic pack pattern

--- src/LicenseChecker.go
+++ src/LicenseChecker.go
@@ -77,7 +77,7 @@ func (checker LicenseChecker) GetLicenseMeta(presetPath string) (licenseTierReq 
 			switch celestionType {
 			case 4, 5, 6, 7, 8, 9, 19, 20, 21, 22, 23, 24, 28, 29, 39:
 				licenseTierReq.ModernVintage = true
-			case 1, 2, 3, 10, 11, 12, 13, 14, 15, 16, 17, 18, 25, 26, 27:
+			case 1, 2, 3, 10, 11, 12, 13, 14, 16, 18, 25, 26, 27:
 				licenseTierReq.CelestionClassic = true
 			}
 		}
